54f11c4d16840afb86097b9aee0524c758eed593
BoshEee <bosheee.shwi@gmail.com>
2021-01-30 15:33:14 +0200

    autocomplete feature

--- public/index.js
+++ public/index.js
@@ -1,5 +1,15 @@
+console.clear();
+
 const searchInput = document.querySelector("input");
 const datalist = document.querySelector("datalist");
+const searchBtn = document.querySelector("button");
+const info = document.querySelector(".info");
+const title = document.querySelector("#title");
+const Platform = document.querySelector("#platform");
+const genres = document.querySelector("#genres");
+const poster = document.querySelector("#poster");
+
+searchBtn.addEventListener("click", getinfo);
 
 searchInput.addEventListener("keyup", (event) => {
   datalist.innerHTML = "";
@@ -20,3 +30,31 @@ searchInput.addEventListener("keyup", (event) => {
       console.log(error);
     });
 });
+
+function getinfo(event) {
+  event.preventDefault();
+
+  if (searchInput.value == "") {
+    alert("You Need To Type A Game Name");
+  } else {
+    let game = searchInput.value;
+    console.log("btn : " + game);
+
+    fetch(`/search/?name=${game}`)
+      .then((response) => {
+        if (!response.ok) throw new Error(response.status);
+        return response.json();
+      })
+      .then((result) => {
+        info.style.display = "block";
+        title.textContent = result[0].name;
+        platform.textContent = "Platforms : " + result[0].platform;
+        genres.textContent = "Genres : " + result[0].genres;
+        poster.src = result[0].poster;
+        console.log("name : " + result[0].genres);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+}
