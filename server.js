e2337d4d01dd5d2285abf1e83e80232c3d51adc5
BoshEee <bosheee.shwi@gmail.com>
2021-01-30 12:31:11 +0200

    testing heroku

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const http = require("http");
+const PORT = 3000;
 
 const server = http.createServer((request, response) => {
-  response.end("Hello World!");
+  response.writeHead(200, { "content-type": "text/html" });
+  response.end("<h1>Hello World!</h1>");
 });
+
+server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
